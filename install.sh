37c3a17987625af1ef642495e4926d7469ce96ef
Pawel Pekala <c0rn@o2.pl>
2017-01-13 21:15:33 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+srcdir=$(realpath "${0%/*}")
+homedir=$(realpath "$HOME")
+confdir=${srcdir#$homedir/}
+
+cd "$HOME"
+for config in conkyrc mostrc vimrc zshrc; do
+	[ -e .$config ] || ln -sv "$confdir/$config" .$config
+done
+
+if ! which git >/dev/null; then
+	echo "Git not found, skipping Vundle VIM plugin install."
+	exit
+fi
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+	echo "Downloading Vundle VIM plugin..."
+	git clone https://github.com/VundleVim/Vundle.vim.git \
+	       "$HOME/.vim/bundle/Vundle.vim"
+	if [ $? -eq 0 ]; then
+		echo "Vundle installed, do :PluginInstall in vim to install other plugins."
+	else
+		echo "Vundle install failed."
+	fi
+fi
